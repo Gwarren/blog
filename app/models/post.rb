7a991d9a28e5786c052e92b9a5c3a005e74e86c1
lkdjiin <xavier.nayrac@gmail.com>
2013-05-21 10:00:52 +0200

    Add basic sidebar

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,11 +6,18 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 255 }
   validates :content, presence: true
 
-  # Get the 30 first words of the post's content, taking care of HTML_Truncator
-  # tags.
+  # Public: Get the 30 first words of the post's content, taking care of
+  # html tags.
   #
   # Returns a String, the truncated post content.
   def truncated_content
     HTML_Truncator.truncate(markdown(content), 30).html_safe
+  end
+
+  # Public: Get the tags of this post ready to be display in html.
+  #
+  # Returns a html formatted String.
+  def labeled_tags
+    tags.map{|t| "<span class='label'>#{t.label}</span>"}.join(' ').html_safe
   end
 end
