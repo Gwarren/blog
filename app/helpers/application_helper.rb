1aad5c2f8addf73d74d532e4293b45687934b6e0
lkdjiin <xavier.nayrac@gmail.com>
2013-05-22 17:42:41 +0200

    Add window title and order posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: 'Xavier Nayrac', class: "gravatar")
   end
+
+  def full_title(page_title)
+    base_title = "lkdjiin's blog"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} - #{page_title}"
+    end
+  end
 end
